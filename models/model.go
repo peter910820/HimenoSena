128405e0210ebf9f13920d1c8fd2bf7e01d99293
peter910820 <peter910820@gmail.com>
2025-07-09 20:48:02 +0800

    feat: make user model

--- models/model.go
+++ models/model.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -13,4 +15,13 @@ type Config struct {
 	BotChannelID2 string
 	VoiceManageID string
 	DevCategoryID string
+}
+
+type User struct {
+	UserID    string    `gorm:"primaryKey"`
+	ServerID  string    `gorm:"primaryKey"`
+	Level     uint      `gorm:"not null;default:1"`
+	Exp       uint      `gorm:"not null;default:0"`
+	JoinTime  time.Time `gorm:"not null"`
+	UpdatedAt time.Time
 }
