a86a0adf91b4e449496133ef56987de5ba7483bb
SeaotterMS <peter910820@gmail.com>
2026-01-09 23:54:28 +0800

    fix: fixed the error that the log was too long to be displayed

--- commands/get_log.go
+++ commands/get_log.go
@@ -7,42 +7,65 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
 )
 
 func GetLog(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	const lastNLines = 10
+	const lastNLines = 20
 	file, err := os.Open(os.Getenv("KUROHELPER_LOG_PATH"))
 	if err != nil {
+		logrus.Error(err)
 		utils.SendInteractionMsg(s, i, "")
 		return
 	}
 	defer file.Close()
 
+	const maxLineLength = 250
 	var lines []string
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		line := scanner.Text()
+		// 如果一行超過250字元，只取前250字元並加上...
+		if len(line) > maxLineLength {
+			line = line[:maxLineLength] + "..."
+		}
+		lines = append(lines, line)
 		if len(lines) > lastNLines {
 			lines = lines[1:]
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
+		logrus.Error(err)
 		utils.SendInteractionMsg(s, i, "")
 		return
 	}
 
+	const linesPerField = 5
+	var fields []*discordgo.MessageEmbedField
+
+	// 每5行一個 field
+	for i := 0; i < len(lines); i += linesPerField {
+		end := i + linesPerField
+		if end > len(lines) {
+			end = len(lines)
+		}
+
+		fieldLines := lines[i:end]
+		fieldValue := strings.Join(fieldLines, "\n")
+
+		fields = append(fields, &discordgo.MessageEmbedField{
+			Name:   "",
+			Value:  fieldValue,
+			Inline: false,
+		})
+	}
+
 	embed := &discordgo.MessageEmbed{
-		Title: "KuroHelper Log紀錄(最後10筆)",
-		Color: 0x60373E,
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:   "Log",
-				Value:  strings.Join(lines, "\n"),
-				Inline: false,
-			},
-		},
+		Title:  "KuroHelper Log紀錄(最後10筆)",
+		Color:  0x60373E,
+		Fields: fields,
 	}
 
 	utils.SendEmbedInteractionMsg(s, i, embed)
